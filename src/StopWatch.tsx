696620d6a0d285d3604da620ee08dd9c4cdc0ea1
Terrence <terrence.liu@alumni.ubc.ca>
2024-01-22 23:08:27 -0800

    Added initial state management for stopwatch/reset

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch() {
+    const [count, setCount] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+    const [laps, setLaps] = useState([]);
+
     return(
-        <div></div>
+        <section className={"stopwatch"}>
+            {count}
+            <StopWatchButton
+            count={count}
+            setCount={setCount}
+            isActive={isActive}
+            setIsActive={setIsActive}
+            />
+        </section>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-export default function StopWatchButton() {
+type StopWatchButtonProps = {
+    isActive: boolean;
+    count: number;
+    setCount: React.Dispatch<React.SetStateAction<number>>;
+    setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const stopWatchHandler = (
+    setIsActive: React.Dispatch<React.SetStateAction<boolean>>
+    ) => {
+    setIsActive((prev) => !prev);
+};
+
+const resetWatchHandler = (
+    setCount: React.Dispatch<React.SetStateAction<number>>,
+) => {
+    setCount(0);
+};
+
+const lapHandler = () => {
+};
+
+export default function StopWatchButton({
+    count,
+    isActive,
+    setCount,
+    setIsActive
+}: StopWatchButtonProps) {
     return(
-        <div></div>
+        <div>
+            <button onClick={() => stopWatchHandler(setIsActive)}>{!isActive ? count === 0 ? "Start" : "Resume" : "Pause"}</button>
+            <button onClick={() => resetWatchHandler(setCount)}>Reset</button>
+        </div>
     )
 }
